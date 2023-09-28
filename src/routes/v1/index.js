9c5335e560dc55d74af492f1e7830429390dced8
Sajid Khan <mdskhan4774@gmail.com>
2023-09-28 20:36:02 +0530

    Added signup with encrypted password

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { createTweet, getTweet} from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 import { createComment } from '../../controllers/comment-controller.js';
+import { signup} from '../../controllers/auth-controller.js';
 
 
 
@@ -14,7 +15,7 @@ router.get('/tweets/:id', getTweet);
 router.post('/likes/toggle',toggleLike);
 router.post('/comments', createComment);
 
-
+router.post('/signup', signup);
 
 
 export default router;
